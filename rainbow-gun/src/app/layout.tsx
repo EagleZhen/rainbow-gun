946fe0251b85bc1fd9066753989faa46d4918684
EZ <31062364+EagleZhen@users.noreply.github.com>
2025-11-26 18:36:31 +0800

    feat: add colorful favicons
    source: https://www.flaticon.com/free-icon/megaphone_7748773

--- rainbow-gun/src/app/layout.tsx
+++ rainbow-gun/src/app/layout.tsx
@@ -15,6 +15,14 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Rainbow Gun",
   description: "AIST2010 Group Project - Rainbow Gun",
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({
